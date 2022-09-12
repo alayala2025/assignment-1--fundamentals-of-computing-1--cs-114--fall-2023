2a38589b75ca4314f3e74c4e4231282881ba4c5f
bsalchert <salchert@hartford.edu>
2022-09-12 10:11:10 -0400

    Moved variable declarations & changed case

--- Assignment1.java
+++ Assignment1.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Assignment1 {
     public static void main(String[] args) {
         final double CONVERSION_FACTOR = (5.0 / 9.0);       //Used to convert Fahrenheit to Celsius and vice versa
+        float tempFahrenheit;
+        float tempCelsius;
+        String modifiedString;
+        
         System.out.println("BBBBBBB      SSSSSSS ");        //Prints the block-letter initials as seen in the code
         System.out.println("BBB   BBB   SSS   SSS");
         System.out.println("BBB   BBB   SSS      ");
@@ -12,9 +16,9 @@ public class Assignment1 {
         System.out.println("BBBBBBB      SSSSSSS ");
         Scanner scan = new Scanner(System.in);              //Scanner used to obtain user input
         System.out.println("Please enter a number in Fahrenheit:");
-        float TempFahrenheit = scan.nextFloat();
-        float TempCelsius = (float) ((TempFahrenheit - 32) * CONVERSION_FACTOR); //Converts Fahrenheit to Celsius
+        tempFahrenheit = scan.nextFloat();
+        tempCelsius = (float) ((TempFahrenheit - 32) * CONVERSION_FACTOR); //Converts Fahrenheit to Celsius
         System.out.println("Please enter a 5-character string:");
-        String ModifiedString = "";
+        modifiedString = "";
     }
 }
