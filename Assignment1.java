8b9c02874f4d4f545a94f845f558c5726b4654e4
bsalchert <salchert@hartford.edu>
2022-09-12 09:52:39 -0400

    Copied Assignment from local

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,20 @@
-// Import libraries here
+import java.util.Scanner;
 
 public class Assignment1 {
     public static void main(String[] args) {
-        // Add your code here
+        final double ConversionFactor = (5.0 / 9.0);   //Used to convert Fahrenheit to Celsius and vice versa
+        System.out.println("BBBBBBB      SSSSSSS ");      //Prints the block-letter initials as seen in the code
+        System.out.println("BBB   BBB   SSS   SSS");
+        System.out.println("BBB   BBB   SSS      ");
+        System.out.println("BBBBBBB      SSSSSSS ");
+        System.out.println("BBB   BBB         SSS");
+        System.out.println("BBB   BBB   SSS   SSS");
+        System.out.println("BBBBBBB      SSSSSSS ");
+        Scanner scan = new Scanner(System.in);              //Scanner used to obtain user input
+        System.out.println("Please enter a number in Fahrenheit:");
+        float TempFahrenheit = scan.nextFloat();
+        float TempCelsius = (float) ((TempFahrenheit - 32) * ConversionFactor); //Converts Fahrenheit to Celsius
+        System.out.println("Please enter a 5-character string:");
+        String ModifiedString = "";
     }
 }
