6fa7dbf832ccf072fcf4f7a4beaca2a181df2205
bsalchert <salchert@hartford.edu>
2022-09-12 10:44:07 -0400

    Cleaned comment start spacing

--- Assignment1.java
+++ Assignment1.java
@@ -2,14 +2,14 @@ import java.util.*; // Used to import the Scanner and Random classes
 
 public class Assignment1 {
     public static void main(String[] args) {
-        final double CONVERSION_FACTOR = (5.0 / 9.0);       //Used to convert Fahrenheit to Celsius and vice versa
-        Scanner scan = new Scanner(System.in);              //Scanner used to obtain user input
+        final double CONVERSION_FACTOR = (5.0 / 9.0);       // Used to convert Fahrenheit to Celsius and vice versa
+        Scanner scan = new Scanner(System.in);              // Scanner used to obtain user input
         float tempFahrenheit;
         float tempCelsius;
         String inputString;
         String modifiedString;
         
-        System.out.println("BBBBBBB      SSSSSSS ");      //Prints the block-letter initials as seen in the code
+        System.out.println("BBBBBBB      SSSSSSS ");      // Prints the block-letter initials as seen in the code
         System.out.println("BBB   BBB   SSS   SSS");
         System.out.println("BBB   BBB   SSS      ");
         System.out.println("BBBBBBB      SSSSSSS ");
@@ -18,7 +18,7 @@ public class Assignment1 {
         System.out.println("BBBBBBB      SSSSSSS ");
         System.out.println("Please enter a number in Fahrenheit:");
         tempFahrenheit = scan.nextFloat();
-        tempCelsius = (float) ((tempFahrenheit - 32) * CONVERSION_FACTOR); //Converts Fahrenheit to Celsius
+        tempCelsius = (float) ((tempFahrenheit - 32) * CONVERSION_FACTOR); // Converts Fahrenheit to Celsius
         System.out.println("Please enter a 5-character string:");
         inputString = scan.next();
     }
