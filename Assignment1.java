417426dc93605210a176b7bc3181f1676fd65067
bsalchert <salchert@hartford.edu>
2022-09-12 10:19:33 -0400

    Moved Scanner object to top of code

--- Assignment1.java
+++ Assignment1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Assignment1 {
     public static void main(String[] args) {
         final double CONVERSION_FACTOR = (5.0 / 9.0);       //Used to convert Fahrenheit to Celsius and vice versa
+        Scanner scan = new Scanner(System.in);              //Scanner used to obtain user input
         float tempFahrenheit;
         float tempCelsius;
         String inputString;
@@ -15,7 +16,6 @@ public class Assignment1 {
         System.out.println("BBB   BBB         SSS");
         System.out.println("BBB   BBB   SSS   SSS");
         System.out.println("BBBBBBB      SSSSSSS ");
-        Scanner scan = new Scanner(System.in);              //Scanner used to obtain user input
         System.out.println("Please enter a number in Fahrenheit:");
         tempFahrenheit = scan.nextFloat();
         tempCelsius = (float) ((tempFahrenheit - 32) * CONVERSION_FACTOR); //Converts Fahrenheit to Celsius
