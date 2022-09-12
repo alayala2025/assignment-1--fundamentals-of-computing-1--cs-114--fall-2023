557a6d6a8bdc3e2f9a97b0526a5934ad848f7407
bsalchert <salchert@hartford.edu>
2022-09-12 10:09:50 -0400

    Updated constant capitalization

--- Assignment1.java
+++ Assignment1.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Assignment1 {
     public static void main(String[] args) {
-        final double ConversionFactor = (5.0 / 9.0);   //Used to convert Fahrenheit to Celsius and vice versa
-        System.out.println("BBBBBBB      SSSSSSS ");      //Prints the block-letter initials as seen in the code
+        final double CONVERSION_FACTOR = (5.0 / 9.0);       //Used to convert Fahrenheit to Celsius and vice versa
+        System.out.println("BBBBBBB      SSSSSSS ");        //Prints the block-letter initials as seen in the code
         System.out.println("BBB   BBB   SSS   SSS");
         System.out.println("BBB   BBB   SSS      ");
         System.out.println("BBBBBBB      SSSSSSS ");
@@ -13,7 +13,7 @@ public class Assignment1 {
         Scanner scan = new Scanner(System.in);              //Scanner used to obtain user input
         System.out.println("Please enter a number in Fahrenheit:");
         float TempFahrenheit = scan.nextFloat();
-        float TempCelsius = (float) ((TempFahrenheit - 32) * ConversionFactor); //Converts Fahrenheit to Celsius
+        float TempCelsius = (float) ((TempFahrenheit - 32) * CONVERSION_FACTOR); //Converts Fahrenheit to Celsius
         System.out.println("Please enter a 5-character string:");
         String ModifiedString = "";
     }
