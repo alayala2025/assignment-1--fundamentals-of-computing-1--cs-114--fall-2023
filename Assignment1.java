808adb76f71cd7868933fe1fd9d1d6eb0b1f483a
CamDas <111987807+CamDas@users.noreply.github.com>
2022-09-14 15:21:28 -0400

    Scanner
    Started to add scanner

--- Assignment1.java
+++ Assignment1.java
@@ -1,13 +1,23 @@
-// Import libraries here
+import java.util.Scanner;  // Import the Scanner class
 
 public class Assignment1 {
     public static void main(String[] args) {
-        System.out.println ("  CCCCCC       DDDDDD   ");
+        System.out.println ("  CCCCCC       DDDDDD   ");              //Prints initials
         System.out.println (" CCC           DD    DD ");
         System.out.println ("CCC            DD    DD ");
         System.out.println ("CCC            DD    DD ");
         System.out.println ("CCC            DD    DD ");
         System.out.println (" CCC           DD    DD ");
         System.out.println ("  CCCCCC       DDDDDD   ");
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print ("Enter your name, please: ");
+        Name = scan.nextInt();
+        Stringnext()
+
+
+        System.out.println ("Please enter a 5-character string:");
+
     }
 }
