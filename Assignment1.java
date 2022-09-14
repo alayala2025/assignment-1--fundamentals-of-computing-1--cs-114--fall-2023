edf5e482cdb91cf3946decc60fbdc1c5d5a04178
justin-jaques <87876422+V1ridescent@users.noreply.github.com>
2022-09-14 08:19:36 -0400

    Fixed variable names/Formatting

--- Assignment1.java
+++ Assignment1.java
@@ -1,21 +1,18 @@
-//Justin Jaques - Assignment 1
 import java.util.*;
 
-
+//Justin Jaques - Assignment 1
 
 public class Assignment1 {
-
     public static Scanner scan = new Scanner(System.in);
     public static double tempFarenheit;
     public static double tempCelsius;  //Output will be stored in this variable
     public static String originalString; //Input for String
-    public static String cutString; //Cut version of Input String
-    public static String finalString;
+    public static String subString; //Cut version of Input String
 
-    public static Random r = new Random();
+
+    public static Random random = new Random();
     public static int randomLower = 31;
     public static int randomUpper = 16385;
-
 
     //Writing all those System.out.println() lines in the main method makes me physically ill
     public static void nameDisplay() {
@@ -29,14 +26,14 @@ public class Assignment1 {
         System.out.println("JJJJJJJJJJJ\t\tJJJJJJJJJJJ");
     }
 
-
     //I got bored and wanted to make my program more aesthetically pleasing.
     public static void typeWriter(String string) {
-        for(int i = 0; i < string.length(); i++){
+        for(int i = 0; i < string.length(); i++) {
             System.out.printf("%c", string.charAt(i));
+
             try {
                 Thread.sleep(100); //Pause time in between characters
-            } catch(InterruptedException ex){
+            } catch(InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
         }
@@ -44,20 +41,18 @@ public class Assignment1 {
 
    //Instead of putting all this code in the main method, I decided to put it in a function
    public static void start() {
-        nameDisplay();
-        typeWriter("Please enter a 5 character string:\n");
-        originalString = scan.next();
-        cutString = originalString.substring(1, 4); //Cuts the string
-        finalString = new StringBuilder(cutString).reverse().toString(); // Reverses the cut string
-        typeWriter("Please enter a temperature in Farenheit:\n");
-        tempFarenheit = scan.nextInt();
-        tempCelsius = (tempFarenheit - 32) * .5556; //Equation for Farenheit to Celsius
-        int random = r.nextInt(randomLower, randomUpper); //Init random number
-        typeWriter("Random number generated...\n");
-        typeWriter("Your new string is: " + tempCelsius + finalString + random);
+       nameDisplay();
+       typeWriter("Please enter a 5 character string:\n");
+       originalString = scan.next();
+       subString = originalString.substring(1, 4); //Cuts the string
+       subString = new StringBuilder(subString).reverse().toString(); // Reverses the cut string
+       typeWriter("Please enter a temperature in Farenheit:\n");
+       tempFarenheit = scan.nextInt();
+       tempCelsius = (tempFarenheit - 32) * .5556; //Equation for Farenheit to Celsius
+       int randomNumber = random.nextInt(randomLower, randomUpper); //Init random number
+       typeWriter("Random number generated...\n");
+       typeWriter("Your new string is: " + tempCelsius + subString + randomNumber);
     }
-
-
     public static void main(String[] args) {
         start();
     }
