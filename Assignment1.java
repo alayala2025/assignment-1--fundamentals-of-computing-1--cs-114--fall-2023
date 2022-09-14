a10978aee9835bad830fea3db9f2d104946300fa
EfrayimG <gesin@hartford.edu>
2022-09-14 08:21:52 -0400

    Added Part 1

--- Assignment1.java
+++ Assignment1.java
@@ -3,5 +3,15 @@
 public class Assignment1 {
     public static void main(String[] args) {
         // Add your code here
+        System.out.println (" \tEEEEEEEEEEE\tGGGGGGGGGGG") ;
+        System.out.println (" \tEEE\t\tGGG") ;
+        System.out.println (" \tEEE\t\tGGG") ;
+        System.out.println (" \tEEE\t\tGGG") ;
+        System.out.println (" \tEEEEEEEEE\tGGG\tGGG") ;
+        System.out.println (" \tEEE\t\tGGG\tGGG") ;
+        System.out.println (" \tEEE\t\tGGG\tGGG") ;
+        System.out.println (" \tEEE\t\tGGG\tGGG") ;
+        System.out.println (" \tEEEEEEEEEEE\tGGGGGGGGGGG") ;
+        System.out.println ();
     }
 }
