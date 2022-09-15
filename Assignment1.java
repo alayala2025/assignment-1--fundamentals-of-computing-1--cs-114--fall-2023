5e8d3ea9c8591c6cfa91796f3e0f2f8c022e3c38
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-15 19:01:22 -0400

    Initial Commit - addded println

--- Assignment1.java
+++ Assignment1.java
@@ -3,5 +3,12 @@
 public class Assignment1 {
     public static void main(String[] args) {
         // Add your code here
+        System.out.println("AAAA");
+        System.out.println("AAAA");
+
+
+
+
+
     }
 }
