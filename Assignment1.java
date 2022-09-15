a9609470471cdd4e1df43a789b5cac21823d9484
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-15 19:19:15 -0400

    Block AK works (part 1)

--- Assignment1.java
+++ Assignment1.java
@@ -2,14 +2,15 @@
 
 public class Assignment1 {
     public static void main(String[] args) {
-        // Add your code here
+        // Prints Block "AK"
         System.out.println();
-        System.out.println("\s\s\sAAAAAA");
-        System.out.println("\s\sAAAAAAAA");
-        System.out.println("AAAA\tAAAA");
-        System.out.println("AAAAAAAAAAAA");
-        System.out.println("AAAA\tAAAA");
-        System.out.println("AAAA\tAAAA");
+        System.out.println("\s\s\sAAAAAA\tKKKK\t\sKKK");
+        System.out.println("\s\sAAAAAAAA\tKKKK\tKKK");
+        System.out.println("AAAA\tAAAA\tKKKKKKKKK");
+        System.out.println("AAAAAAAAAAAA\tKKKKKKKK");
+        System.out.println("AAAA\tAAAA\tKKKK\s\sKKK");
+        System.out.println("AAAA\tAAAA\tKKKK\tKKKK");
+
 
 
 
