5f6b41a914136d276aea61126e7e96f89f7a893c
11jwp03 <11swp03@gmail.com>
2022-09-16 18:02:36 -0400

    added scan and random test

--- Assignment1.java
+++ Assignment1.java
@@ -1,9 +1,11 @@
 // Import libraries here
-import java.util.Scanner;
+import java.util.*;
 
 public class Assignment1 {
     public static void main(String[] args) {
         float fahrenheit;
+        short Num;
+        short RanNum;
 
         System.out.println("      JJ\tPPPPPPPPPPP");
         System.out.println("      JJ\tPP      PPPP");
@@ -16,10 +18,17 @@ public class Assignment1 {
         System.out.println(" J    J \tPP");
         System.out.println("  JJJJ  \tPP");
         
-        Scanner scan = new Scanner(System.in);
+        Scanner scanT = new Scanner(System.in);
         System.out.print("Enter a temprature in Fahrenheit: ");
-        fahrenheit = scan.nextFloat();
-
+        fahrenheit = scanT.nextFloat();
         System.out.println("Temp in Celcius: " + ((fahrenheit-32)*5/9));
+
+        Scanner scanRN = new Scanner(System.in);
+        System.out.print("Enter a number between 31 - 16,385");
+        Num = scanRN.nextFloat();
+        Random genorator = new Random();
+        RanNum = genorator.nextFloat(16352) + 32;
+        System.out.println("Random Number: " + RanNum);
+
     }
 }
