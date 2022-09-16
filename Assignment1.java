bddcec15489c74aa9829a0129d2a173f18236399
11jwp03 <11swp03@gmail.com>
2022-09-16 13:51:03 -0400

    Adjusted J

--- Assignment1.java
+++ Assignment1.java
@@ -2,16 +2,16 @@
 
 public class Assignment1 {
     public static void main(String[] args) {
-        System.out.println("      JJ");
-        System.out.println("      JJ");
-        System.out.println("      JJ");
-        System.out.println("      JJ");
-        System.out.println("      JJ");
-        System.out.println("      JJ");
-        System.out.println("      JJ");
-        System.out.println("      JJ");
-        System.out.println("     JJ");
-        System.out.println("    JJ");
-        System.out.println("   JJ");
+        System.out.println("              J");
+        System.out.println("              J");
+        System.out.println("              J");
+        System.out.println("              J");
+        System.out.println("              J");
+        System.out.println("              J");
+        System.out.println("              J");
+        System.out.println("              J");
+        System.out.println("     JJJ      J");
+        System.out.println("        JJJJ");
+        
     }
 }
