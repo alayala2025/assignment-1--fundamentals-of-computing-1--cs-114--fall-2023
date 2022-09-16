e5ef20b2456c61a348e7b9d68a87f19a7bc7f74e
11jwp03 <11swp03@gmail.com>
2022-09-16 15:16:49 -0400

    added temp scan

--- Assignment1.java
+++ Assignment1.java
@@ -1,4 +1,5 @@
 // Import libraries here
+import java.util.Scanner;
 
 public class Assignment1 {
     public static void main(String[] args) {
@@ -15,5 +16,11 @@ public class Assignment1 {
         System.out.println("JJ    JJ\tPP");
         System.out.println(" J    J \tPP");
         System.out.println("  JJJJ  \tPP");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a temprature in Fahrenheit: ");
+        fahrenheit = scan.nextInt();
+
+       
     }
 }
