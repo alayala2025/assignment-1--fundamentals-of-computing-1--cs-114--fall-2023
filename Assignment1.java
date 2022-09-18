8ccee26870913e6cd548b34f8a36035a09531881
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-09-18 19:30:53 -0400

    Update Assignment1.java
    Added my second initial N for Nicholas

--- Assignment1.java
+++ Assignment1.java
@@ -3,12 +3,12 @@
 public class Assignment1 {
     public static void main(String[] args) {
         // Add your code here
-        System.out.println("KKK\tKkK");
-        System.out.println("KKK " + "KKk");
-        System.out.println("KKKKK");
-        System.out.println("KKK " + "KKk");
-        System.out.println("KKK\tKkK");
+        System.out.println("KKK\tKKK\t\tNNN\t\tNNN");
+        System.out.println("KKK " + "KKK\t\t\tNNNNNNN\t\tNNN");
+        System.out.println("KKKKK\t\t\tNNNNNNNNN\tNNN");
+        System.out.println("KKK " + "KKK\t\t\tNNNN\tNNNNNNN\tNNN");
+        System.out.println("KKK\tKKK\t\tNNN\t\tNNN");
         //My first initial K for Kamau
-        
+
     }
 }
