6202fbeab70b32d128c6642513b5528c3617eb78
CamDas <111987807+CamDas@users.noreply.github.com>
2022-09-17 21:54:16 -0400

    Complete
    Added random number generator, cleaned code

--- Assignment1.java
+++ Assignment1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.Random; //Import the random number generator
 
 public class Assignment1 {
     public static void main(String[] args) {
@@ -10,20 +11,11 @@ public class Assignment1 {
         System.out.println (" CCC           DD    DD ");
         System.out.println ("  CCCCCC       DDDDDD\n ");
 
-        double Fahrenheit;
-        double Celsius;
-        final double CONVERSION_FACTOR = 9.0 / 5.0;                          //Fahrenheit to Celcius formula
-        System.out.println ("Please enter a temperature in Farenheit:");
-        Scanner scan = new Scanner(System.in);
-        Fahrenheit = scan.nextInt();
-
-
-        Celsius = ((Fahrenheit-32)*5)/9;                                     //Calculation after input
-
 
 
         String Five;                                                     //Variable for 5 character input string
         System.out.println ("Please enter a 5-character string:");
+        Scanner scan = new Scanner(System.in);
         Five = scan.nextLine();                                              //Stores input
 
         String FiveAfter = Five.substring(1,4);          //FiveAfter clones the 5-character string
@@ -33,6 +25,25 @@ public class Assignment1 {
                     {FiveTrim += FiveAfter.charAt(i);}
 
 
+
+        double Fahrenheit;
+        double Celsius;
+        final double CONVERSION_FACTOR = 9.0 / 5.0;                          //Fahrenheit to Celcius formula
+        System.out.println ("Please enter a temperature in Farenheit:");
+
+        Fahrenheit = scan.nextInt();
+
+        Celsius = ((Fahrenheit-32)*5)/9;                                     //Calculation after input
+
+
+
+        Random generator = new Random();                                //Calls random number generator
+        int RandomNum;
+
+        RandomNum = generator.nextInt(16384) + 32;                     //Generators a number inside and including the parameters
+        System.out.println ("Random number generated. Continuing...");
+
+        System.out.println ("Your new string is:" + Celsius + FiveTrim + RandomNum); //Final string
     }
 }
 
