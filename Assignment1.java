feb5ea55f2dd065ddc5601b125af50de372459cb
CamDas <111987807+CamDas@users.noreply.github.com>
2022-09-17 21:17:38 -0400

    String reversal
    Added string reversal

--- Assignment1.java
+++ Assignment1.java
@@ -10,15 +10,30 @@ public class Assignment1 {
         System.out.println (" CCC           DD    DD ");
         System.out.println ("  CCCCCC       DDDDDD\n ");
 
+        double Fahrenheit;
+        double Celsius;
+        final double CONVERSION_FACTOR = 9.0 / 5.0;                          //Fahrenheit to Celcius formula
+        System.out.println ("Please enter a temperature in Farenheit:");
+        Scanner scan = new Scanner(System.in);
+        Fahrenheit = scan.nextInt();
 
 
-        String Five;                                                    //Variable for 5 character input string
-        Scanner scan = new Scanner(System.in);                          //Creates object
+        Celsius = ((Fahrenheit-32)*5)/9;                                     //Calculation after input
+
+
+
+        String Five;                                                     //Variable for 5 character input string
         System.out.println ("Please enter a 5-character string:");
-        Five = scan.nextLine();                                         //Stores input
+        Five = scan.nextLine();                                              //Stores input
 
-        Five.substring(1,5);
+        String FiveAfter = Five.substring(1,4);          //FiveAfter clones the 5-character string
+        String FiveTrim = "";                                                 //FiveTrim is empty to store FiveAfter
+
+        for (int i = 2; i >=0; i--)                                     //reverses characters from FiveAfter and sends into FiveTrim
+                    {FiveTrim += FiveAfter.charAt(i);}
 
 
     }
 }
+
+
