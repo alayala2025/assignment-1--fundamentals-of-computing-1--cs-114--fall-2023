37c0afce5ee28c8e12c5c518ab229469ac2e6e53
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-09-18 19:08:12 -0400

    Update Assignment1.java
    Making my first initial K

--- Assignment1.java
+++ Assignment1.java
@@ -3,5 +3,10 @@
 public class Assignment1 {
     public static void main(String[] args) {
         // Add your code here
+        System.out.println("KKK\tKkK");
+        System.out.println("KKK " + "KKk");
+        System.out.println("KKKKK");
+        System.out.println("KKK " + "KKk");
+        System.out.println("KKK\tKkK");
     }
 }
