7974edb660646e1c7cc7f405f7731e0a80f9460f
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-19 09:08:12 -0400

    changed spaces (4) to indents

--- Assignment1.java
+++ Assignment1.java
@@ -3,50 +3,49 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Assignment1 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		//Variables and Declarations
+		String input;
+		Scanner FiveChar = new Scanner(System.in);
 
-        //Variables and Declarations
-        String input;
-        Scanner FiveChar = new Scanner(System.in);
+		int celsius;
+		int fahrenheit;
+		Scanner Temp = new Scanner(System.in);
 
-        int celsius;
-        int fahrenheit;
-        Scanner Temp = new Scanner(System.in);
+		Random generator = new Random();
+		int randNum;
 
-        Random generator = new Random();
-        int randNum;
+		// Prints Block "AK"
+		System.out.println();
+		System.out.println("\s\s\sAAAAAA\tKKKK\t\sKKK");
+		System.out.println("\s\sAAAAAAAA\tKKKK\tKKK");
+		System.out.println("AAAA\tAAAA\tKKKKKKKKK");
+		System.out.println("AAAAAAAAAAAA\tKKKKKKKK");
+		System.out.println("AAAA\tAAAA\tKKKK\s\sKKKK");
+		System.out.println("AAAA\tAAAA\tKKKK\tKKKK");
 
-        // Prints Block "AK"
-        System.out.println();
-        System.out.println("\s\s\sAAAAAA\tKKKK\t\sKKK");
-        System.out.println("\s\sAAAAAAAA\tKKKK\tKKK");
-        System.out.println("AAAA\tAAAA\tKKKKKKKKK");
-        System.out.println("AAAAAAAAAAAA\tKKKKKKKK");
-        System.out.println("AAAA\tAAAA\tKKKK\s\sKKKK");
-        System.out.println("AAAA\tAAAA\tKKKK\tKKKK");
+		//5 Character string input from user
+		System.out.println("\nPlease enter a 5-character string:");
+			input = FiveChar.nextLine();
 
-        //5 Character string input from user
-        System.out.println("\nPlease enter a 5-character string:");
-            input = FiveChar.nextLine();
+		//Temperature integer input from user
+		System.out.println("\nPlease enter a number in Fahrenheit:");
+			fahrenheit = Temp.nextInt();
 
-        //Temperature integer input from user
-        System.out.println("\nPlease enter a number in Fahrenheit:");
-            fahrenheit = Temp.nextInt();
+		//Change F to Celsius
+		celsius = (fahrenheit - 32) * 5/9;
 
-        //Change F to Celsius
-        celsius = (fahrenheit - 32) * 5/9;
+		//Modifys the 5 character input (remove first and last letter, than mirror inner three)
+		input = input.substring(1,4);
+			//NEED TO FIND OUT HOW TO FLIP THE STRING IDK HOW
 
-        //Modifys the 5 character input (remove first and last letter, than mirror inner three)
-        input = input.substring(1,4);
-            //NEED TO FIND OUT HOW TO FLIP THE STRING IDK HOW
+		//RNG
+		randNum = generator.nextInt(16384 - 32) + 32;
 
-        //RNG
-        randNum = generator.nextInt(16384 - 32) + 32;
-
-        //Creating the Final String
-        System.out.println("\nYour new string is " + celsius + input + randNum);
-
+		//Creating the Final String
+		System.out.println("\nYour new string is " + celsius + input + randNum);
 
 
-    }
+
+	}
 }
