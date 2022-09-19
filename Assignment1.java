08e14edcc0391fbe7b7ef8ca663c66525752c8f6
maizelstrang <mastrang@hartford.edu>
2022-09-19 18:47:19 -0400

    Added Initials

--- Assignment1.java
+++ Assignment1.java
@@ -2,6 +2,13 @@
 
 public class Assignment1 {
     public static void main(String[] args) {
-        // Add your code here
+        System.out.println("MMM            MMM           SSSSSS");
+        System.out.println("MMM MMM    MMM MMM         SSS     SSS");
+        System.out.println("MMM   MM   MM  MMM         SSS");
+        System.out.println("MMM     MMM    MMM          SSSS");
+        System.out.println("MMM      M     MMM             SSS");
+        System.out.println("MMM            MMM       SSS      SSS");
+        System.out.println("MMM            MMM        SSSS     SSS");
+        System.out.println("MMM            MMM           SSSSSS");
     }
 }
