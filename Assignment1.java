3f247910e3548b1eb16e60041bef07e34f4e2cc0
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-09-18 23:06:44 -0400

    Update Assignment1.java
    Started working on Step 2

--- Assignment1.java
+++ Assignment1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 // Import libraries here
 
 public class Assignment1 {
@@ -10,6 +11,19 @@ public class Assignment1 {
         System.out.println("KKK\tKKK\t\tNNN\t\tNNN");
         //My first initial K for Kamau
         //My second initial N for Nicholas
+        //Step 1 of assignment 1 completed
+        System.out.println();
+        System.out.println();
+        //This is for Step 2 of Assignment 1
+        //I need to ask the user to input a value for fahrenheit and
+        //then translate it into celsuis; so x times 32
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a temperature in Fahrenheit: ");
+        double temperature = scanner.nextInt();
+
+        System.out.println("The temperature is "+temperature+" in Celsius");
+
 
 
     }
