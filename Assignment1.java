84877876d810d697db44f865592f9e22e03d3d72
maizelstrang <mastrang@hartford.edu>
2022-09-20 16:04:42 -0400

    Print temperature in Celcius

--- Assignment1.java
+++ Assignment1.java
@@ -24,12 +24,11 @@ public class Assignment1 {
         String fiveLetterWord2 = fiveLetterWord.substring( 1, 4);
 
         System.out.println("Please enter a number in Fahrenheit:");
-
-
         int temp;
         temp = scanner.nextInt();
         int celcius;
         celcius = (temp - 32) / 5/9;
+        System.out.println("Your temperature in Celcius is: " + celcius);
 
         System.out.println("Random number generated. Continuing...");
 
