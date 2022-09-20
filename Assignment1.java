64c20bac85d86369fb066e5ee9a5bf4ca0d07766
KyraSingh4 <kyra.singh4444@gmail.com>
2022-09-19 20:41:38 -0400

    First push

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,36 @@
-// Import libraries here
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Assignment1 {
     public static void main(String[] args) {
-        // Add your code here
+        System.out.println("KK   KK   SSSS  ");
+        System.out.println("KK  KK  SSS  SSS");
+        System.out.println("KK KK   SS");
+        System.out.println("KKKK      SSSS  ");
+        System.out.println("KK KK         SS");
+        System.out.println("KK  KK  SSS  SSS");
+        System.out.println("KK   KK   SSSS  ");
+
+        Scanner sc = new Scanner(System.in);
+
+        String reverse = "";
+        char fiveChar;
+
+        System.out.print("Enter a 5 letter string: ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter a temperature in Fahrenheit: ");
+        double temp = sc.nextInt();
+
+        for (int i = 0; i < str.length(); i++) {
+            fiveChar = str.charAt(i);
+            reverse = fiveChar + reverse;
+        }
+
+        reverse = reverse.substring(1, 4);
+        int randNum = ThreadLocalRandom.current().nextInt(32, 16385);
+        double convert = (temp - 32) * 5 / 9;
+
+        System.out.println(convert + reverse + randNum);
     }
 }
--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ all: build run
 build:
 	javac Assignment1.java
 run:
-	java Assignment1.java
+	java Assignment1
